5450bd06b930367539de094689162fa1d336c75c
Yulms <yminsk@gmail.com>
2020-03-02 11:31:09 +0300

    Добавлена интерактивная карта. Унифицировано открытие, закрытие модальных окон в JS

--- js/script.js
+++ js/script.js
@@ -1,15 +1,17 @@
 "use strict";
 
-//модальное окно - object. При нажатии на кнопке открытия модального окна записываем в эту переменную
-// конкретный объект окна
+// модальное окно - object. При нажатии на кнопке открытия модального окна записываем в эту переменную
+// объект вызываемого окна
 let modalWindow;
-let buttonCloseModal = document.querySelector(".button-close");
+let buttonClose;
 let buttonModalContact = document.querySelector(".button--modal--contact");
-// переменные формы
-let contactForm = document.querySelector(".contact-form");
+let buttonModalMap = document.querySelector(".button--modal--map");
+// переменные формы контактов
 let contactName = document.querySelector(".contact-form__input--name");
 let contactEmail = document.querySelector(".contact-form__input--email");
 let contactText = document.querySelector(".contact-form__input--letter");
+let contactForm = document.querySelector(".contact-form");
+
 // проверка поддержки local storage
 let isStorageSupport = true;
 let storage = "";
@@ -19,44 +21,71 @@ try {
   isStorageSupport = false;
 }
 
+//
+//
+//
+// НАЗНАЧЕНИЕ ОБРАБОТЧИКОВ НАЖАТИЙ НА КНОПКИ ВЫЗОВА МОДАЛЬНЫХ ОКОН
 buttonModalContact.addEventListener("click", function(evt) {
-  evt.preventDefault();
-  modalWindow = document.querySelector(".modal--contact");
-  modalWindow.classList.add("modal--show");
-
-  // если браузер поддерживает local storage
-  if (isStorageSupport) {
-    // при наличии инфо о юзере подставляем ее в соответствующие поля
-    storage = localStorage.getItem("userName");
-    if (storage) {
-      contactName.value = storage;
-    } else {
-      // устанавливаем фокус на поле имя
-      contactName.focus();
-    }
-
-    storage = localStorage.getItem("userEmail");
-    if (storage) {
-      contactEmail.value = storage;
-    } else {
-      // если имя заполнено устанавливаем фокус на email
-      if (contactName.value) {
-        contactEmail.focus();
-      }
-    }
-  } else {
-    // устанавливаем фокус на поле имя
-    contactName.focus();
-  }
+  windowShow(evt, ".modal--contact");
 });
 
-// Валидация и запись данных формы в local-storage.
+buttonModalMap.addEventListener("click", function(evt) {
+  windowShow(evt, ".modal--map");
+});
+
+function windowShow(evt, formClass) {
+  evt.preventDefault();
+  switch (formClass) {
+    case ".modal--contact":
+      modalWindow = document.querySelector(".modal--contact");
+      modalWindow.classList.add("modal--show");
+      contactText.focus();
+      // если браузер поддерживает local storage
+      if (isStorageSupport) {
+        // при наличии инфо о юзере подставляем ее в соответствующие поля
+        storage = localStorage.getItem("userName");
+        if (storage) {
+          contactName.value = storage;
+        } else {
+          // устанавливаем фокус на поле имя
+          contactName.focus();
+        }
+        storage = localStorage.getItem("userEmail");
+        if (storage) {
+          contactEmail.value = storage;
+        } else {
+          // если имя заполнено устанавливаем фокус на email
+          if (contactName.value) {
+            contactEmail.focus();
+          }
+        }
+      } else {
+        // устанавливаем фокус на поле имя
+        contactName.focus();
+      }
+      break;
+    case ".modal--map":
+      modalWindow = document.querySelector(".modal--map");
+      modalWindow.classList.add("modal--show");
+      break;
+  }
+
+  // в зависимости от открытого модального окна
+  // 1. записываем в переменную объект кнопки закрытия именно открытого окна
+  // 2. вызываем функцию, навешиваюзую обработчик события на эту кнопку
+  buttonClose = document.querySelector(formClass + " .button-close");
+  addEventClose();
+}
+
+//
+//
+// ОТПРАВКА ДАННЫХ. Валидация и запись данных формы в local-storage.
 contactForm.addEventListener("submit", function(evt) {
   if (!contactName.value || !contactEmail.value || !contactText.value) {
     evt.preventDefault();
-    // хак, чтобы анимация ошибки отрабатывала несколько раз
+    // хак, чтобы анимация ошибки отрабатывала несколько раз (просто удаление и добавление класса не сработает)
     modalWindow.classList.remove("modal--error");
-    // операция из демо не сработала popup.offsetWidth = popup.offsetWidth;
+    // операция из лекции не сработала: popup.offsetWidth = popup.offsetWidth;
     +modalWindow.offsetWidth;
     modalWindow.classList.add("modal--error");
   } else {
@@ -67,12 +96,18 @@ contactForm.addEventListener("submit", function(evt) {
   }
 });
 
-// закрытие окна по клику на крестике
-buttonCloseModal.addEventListener("click", function(evt) {
-  evt.preventDefault();
-  modalWindow.classList.remove("modal--show");
-  modalWindow.classList.remove("modal--error");
-});
+//
+//
+//
+// ЗАКРЫТИЕ ОКОН
+// закрытие окна контактов
+function addEventClose() {
+  buttonClose.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    modalWindow.classList.remove("modal--show");
+    modalWindow.classList.remove("modal--error");
+  });
+}
 
 // закрытие окна по нажатию esc
 window.addEventListener("keydown", function(evt) {
